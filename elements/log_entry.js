90e4ad85d9a4ff46cf5fcbf55c0abbca6f9a62a1
James Allen <jamesallen0108@gmail.com>
2011-08-14 12:09:47 +0100

    Toggle display of error content
    For LaTeX errors which have some additional explanation, this is now
    hidden by default in the log view and togglable with a small arrow at
    the side.

--- elements/log_entry.js
+++ elements/log_entry.js
@@ -15,26 +15,52 @@ apf.logentry = function(struct, tagName){
     this.$propHandlers["content"] = function(content) {
         if (!this.$content) return;
         if (content) {
+            this.$hasContent();
+            this.$hideContent();
             this.$content.innerHTML = content;
-            this.$content.style.display = "block";
         }
     };
     
     this.$propHandlers["type"] = function(type) {
         if (!this.$container) return;
-        var curClass = this.$container.getAttribute("class") || "";
-        this.$container.setAttribute("class", curClass + " " + type);
+        apf.setStyleClass(this.$container, type);
     };
     
     this.$draw = function(){
-        this.$ext = this.$getExternal();
-        this.$arrow = this.$getLayoutNode("main", "icon", this.$ext);
-        this.$summary = this.$getLayoutNode("main", "summary", this.$ext);
-        this.$content = this.$getLayoutNode("main", "content", this.$ext);
+        this.$ext       = this.$getExternal();
         this.$container = this.$getLayoutNode("main", "container", this.$ext);
-        
-        this.$content.style.display = "none";
+        this.$toggle    = this.$getLayoutNode("main", "toggle", this.$ext);
+        this.$summary   = this.$getLayoutNode("main", "summary", this.$ext);
+        this.$content   = this.$getLayoutNode("main", "content", this.$ext);
     };
+
+    this.$hasContent = function() {
+        apf.setStyleClass(this.$container, "hasContent");
+
+        var self = this;
+        this.$container.addEventListener("dblclick", function() {
+            self.$toggleContent();
+        });
+        this.$toggle.addEventListener("click", function() {
+            self.$toggleContent();
+        });
+    };
+
+    this.$toggleContent = function() {
+        if (this.$container.className.match("hideContent")) {
+            this.$showContent();
+        } else {
+            this.$hideContent();
+        }
+    }
+
+    this.$hideContent = function() {
+        apf.setStyleClass(this.$container, "hideContent", ["showContent"]);
+    }
+
+    this.$showContent = function() {
+        apf.setStyleClass(this.$container, "showContent", ["hideContent"]);
+    }
 }).call(apf.logentry.prototype = new apf.Presentation());
 
 apf.aml.setElement("logentry", apf.logentry);
